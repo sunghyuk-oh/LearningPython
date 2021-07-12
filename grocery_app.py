a8a9374e052a0a1afc31e7c6f92256c58859d68f
Tim Oh <timoh@Tims-MacBook-Pro.local>
2021-07-12 18:22:46 -0400

    Assignment for Day 5

--- grocery_app.py
+++ grocery_app.py
@@ -1,14 +1,11 @@
-shopping_lst = []
+shopping_lists = []
 
 
 class Store:
-    def __init__(self, store, address):
-        self.store = store
+    def __init__(self, title, address):
+        self.title = title
         self.address = address
-        self.item_list = []
-
-    def add_shopping_list(self, item):
-        self.item_list.append(item)
+        self.item_lists = []
 
 
 class Item:
@@ -22,56 +19,54 @@ while True:
     choice = int(input("""
   Please choose the option:
   1. Add a shopping list
-  2. Display the list
-  3. Quit
+  2. Add grocery items to the particular shopping list
+  3. Display the list
+  4. Quit
   """))
 
     if choice == 1:
-        store_name = input('Please enter the store name: ')
-        store_address = input('Please enter the store address: ')
+        store_name = input("\nEnter the name of the store: ")
+        store_address = input("Enter the address of the store: ")
 
         store = Store(store_name, store_address)
-
-        more_choice = input(
-            "\nDo you want to add grocery items to the particular shopping list? (yes/no) ")
-
-        if more_choice.lower() == "yes":
-            while True:
-                item_name = input("\nPlease enter the name of the item: ")
-                price = input("Please enter the price of the item: ")
-                quantity = input("Please enter the quantity: ")
-
-                item = Item(item_name, price, quantity)
-                store.add_shopping_list(item)
-
-                print(f"\n{item_name} has saved to the list!")
-
-                option = input("\nDo you want to add more items? (yes/no) ")
-                if option.lower() == "yes":
-                    continue
-                else:
-                    break
-        else:
-            continue
-
-        shopping_lst.append(store)
+        shopping_lists.append(store)
 
     elif choice == 2:
-        print("-----------------------------")
-        for i in range(len(shopping_lst)):
-            print(shopping_lst[i].store)
-        print("-----------------------------")
+        print("\n--------------------")
+        for i in range(len(shopping_lists)):
+            print(f"{i+1}. {shopping_lists[i].title}")
+        print("--------------------")
 
-        name_option = input("Enter the store name to display the item list: ")
-        print(f"\nThe grocery items for {name_option} are:")
-        for i in range(len(shopping_lst)):
-            if name_option == shopping_lst[i].store:
-                for item in shopping_lst[i].item_list:
-                    print(f" - {item.title}")
+        option = int(
+            input("\nPlease enter the number according to the list to add the item(s): "))
+
+        while True:
+            name = input("\nPlease enter the name of the item: ")
+            price = input("Please enter the price of the item: ")
+            quantity = input("Please enter the quantity of the item: ")
+
+            item = Item(name, price, quantity)
+
+            shopping_lists[option-1].item_lists.append(item)
+
+            more_choice = input("\nDo you want to add more items? (Yes/No) ")
+
+            if more_choice.lower() == "yes":
+                continue
+            else:
+                break
 
     elif choice == 3:
-        print("Thank you for using the grocery app!")
+        print("\n--------------------")
+        for i in range(len(shopping_lists)):
+            print(shopping_lists[i].title)
+            for item in shopping_lists[i].item_lists:
+                print(f" - {item.title}")
+        print("--------------------")
+
+    elif choice == 4:
+        print("\nThank you for using the grocery app!")
         break
 
     else:
-        print("You typed the wrong number. Please Try again")
+        print("You entered the wrong number. Please try again.")
